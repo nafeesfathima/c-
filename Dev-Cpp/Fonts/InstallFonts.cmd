0be11ccbe02ee82bb2e9bb3d6ec55f8b8c71fae3
nafeesfathima <nafees.fathimaf@gmail.com>
2023-01-05 20:16:48 +0300

    changes

--- Dev-Cpp/Fonts/InstallFonts.cmd
+++
@@ -1,5 +0,0 @@
-echo Should be run as Administrator.
-copy "Heebo-Regular.ttf" "%SYSTEMROOT%\Fonts"
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Heebo Regular (TrueType)" /t REG_SZ /d Heebo-Regular.ttf /f
-copy "SourceCodePro-Regular.ttf" "%SYSTEMROOT%\Fonts"
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Fonts" /v "Source Code Pro (TrueType)" /t REG_SZ /d SourceCodePro-Regular.ttf /f
